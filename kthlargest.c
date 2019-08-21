aca6bdea02345253a7683a31118f2e5312ea0867
jamuna1BM18CS408 <53851857+jamuna1BM18CS408@users.noreply.github.com>
2019-08-21 13:14:06 +0530

    Update kthlargest.c

--- kthlargest.c
+++ kthlargest.c
@@ -5,13 +5,13 @@ int bubblesort(int arr[],int n,int k)
 	for(i=0;i<k;i++)
 	{
 	
-		for(j=0;j<n-2-i;j++)
+		for(j=0;j<n-1-i;j++)
 		{
 			if(arr[j+1]>arr[j])
 		{
-	temp=arr[j];
-	arr[j]=arr[j+1];
-	arr[j+1]=temp;
+	temp=arr[j+1];
+	arr[j+1]=arr[j];
+	arr[j]=temp;
 }
 }
 }
