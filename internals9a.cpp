8a709db316b868f6d7b452806fe60d329983f78f
jamuna1BM18CS408 <53851857+jamuna1BM18CS408@users.noreply.github.com>
2019-10-15 20:37:59 +0530

    Update internals9a.cpp

--- internals9a.cpp
+++ internals9a.cpp
@@ -1,61 +1,66 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int partition(int arr[], int low, int high)
-{
-int pivot=low, i=low+1, j=high;
-
-while(1)
-{
- while(arr[i]<=arr[pivot] && i<=high)
-  i++;
- while(arr[j]>arr[pivot] && j>=low)
-  j--;
- 
- if(i<j)
- {
-  swap(arr[i], arr[j]);
- }
- else
-{
-swap(arr[j], arr[pivot]);
-return j;
-}
-}
-}
-
-int kthsmallest(int a[], int l, int h, int k)
-{
-int pos=partition(a, l, h);
-
-if(pos-l == k-1)
- return a[pos];
-else if(pos-l > k-1)
- return kthsmallest(a, l, pos-1, k);
-
-return kthsmallest(a, pos+1, h, k-pos+l-1);
-}
-
-
-int main()
-{
-int n, i, q;
-cout<<"Enter length of array: ";
-cin>>n;
-int a[n];
-cout<<"Enter array elemnts: ";
-for(i=0; i<n; i++)
-cin>>a[i];
-cout<<"Enter number of queries: ";
-cin>>q;
-
-while(q--)
-{
-int start, end, k;
-cout<<"Enter values of start, end and k: ";
-cin>>start>>end>>k;
-cout<<"The "<<k<<"th smallest element between the given range is: ";
-int ans=kthsmallest(a, start-1, end-1, k);
-cout<<ans<<endl;
-}
-}
+#include <bits/stdc++.h> 
+using namespace std; 
+int binarySearch(int arr[], int low,  
+                  int high, int key) 
+{ 
+  if (high < low) 
+    return -1; 
+      
+  int mid = (low + high)/2; /*low + (high - low)/2;*/
+  if (key == arr[mid]) 
+    return mid; 
+      
+  if (key > arr[mid]) 
+    return binarySearch(arr, (mid + 1), high, key); 
+      
+  // else 
+    return binarySearch(arr, low, (mid -1), key); 
+} 
+int findPivot(int arr[], int low, int high) 
+{ 
+  // base cases 
+  if (high < low) return -1; 
+  if (high == low) return low; 
+  
+   int mid = (low + high)/2; /*low + (high - low)/2;*/
+   if (mid < high && arr[mid] > arr[mid + 1]) 
+    return mid; 
+      
+   if (mid > low && arr[mid] < arr[mid - 1]) 
+    return (mid-1); 
+      
+   if (arr[low] >= arr[mid]) 
+    return findPivot(arr, low, mid-1); 
+      
+   return findPivot(arr, mid + 1, high); 
+} 
+  
+int pivotedBinarySearch(int arr[], int n, int key) 
+{ 
+  int pivot = findPivot(arr, 0, n-1);  
+  if (pivot == -1) 
+    return binarySearch(arr, 0, n-1, key); 
+  
+   
+  if (arr[pivot] == key) 
+    return pivot; 
+      
+  if (arr[0] <= key) 
+    return binarySearch(arr, 0, pivot-1, key); 
+      
+    return binarySearch(arr, pivot+1, n-1, key); 
+} 
+  
+int main() 
+{ 
+  int arr1[20],n,key;
+  cout<<"enter n";
+  cin>>n;
+  for(int i=0;i<n;i++)
+  cin>>arr1[i];
+  cout<<"enter key";
+  cin>>key;    
+  cout << "Index of the element is : " <<pivotedBinarySearch(arr1, n, key); 
+             
+  return 0; 
+} 
